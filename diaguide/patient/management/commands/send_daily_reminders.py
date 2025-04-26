b4af5d72e84c7f55e91435d09c3eb501fd638f7e
Kawtar <kawtartaik123@gmail.com>
2025-04-26 22:40:27 +0100

    changed daily patient reminder email template

--- diaguide/patient/management/commands/send_daily_reminders.py
+++ diaguide/patient/management/commands/send_daily_reminders.py
@@ -1,36 +1,24 @@
 from django.core.management.base import BaseCommand
-from patient.models import Patient, GlucoseRecord, WeightRecord, InsulinRecord
-from django.core.mail import send_mail
+from patient.models import Patient
 from django.utils.timezone import now
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 class Command(BaseCommand):
-    help = 'Send daily reminder emails to patients who did not submit health data today'
 
     def handle(self, *args, **kwargs):
         today = now().date()
 
         patients = Patient.objects.all()
         for patient in patients:
-            missing_data = []
+            subject = "⏰ Daily Health Reminder"
+            from_email = None  
+            to = [patient.user.email]
 
-            if not patient.glucose_records.filter(recorded_at__date=today).exists():
-                missing_data.append('glucose')
+            html_content = render_to_string('emails/daily_reminder.html', {
+                'patient': patient.user,
+            })
 
-            if not patient.weight_records.filter(recorded_at__date=today).exists():
-                missing_data.append('weight')
-
-            if not patient.insulin_records.filter(recorded_at__date=today).exists():
-                missing_data.append('insulin')
-
-            if missing_data:
-                try:
-                    send_mail(
-                        subject="Reminder to record your health data for today",
-                        message=f"Hello {patient.user.nom} {patient.user.prenom},\n\nYou haven't recorded your {', '.join(missing_data)} today. Please update it!",
-                        from_email=None,
-                        recipient_list=[patient.user.email],
-                        fail_silently=False,
-                    )
-                    self.stdout.write(self.style.SUCCESS(f"Reminder sent to {patient.user.email}"))
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(f"Failed to send to {patient.user.email}: {e}"))
+            msg = EmailMultiAlternatives(subject, '', from_email, to)
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
